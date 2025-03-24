4e5660990f7135a6a3e86d3a2f976f918df09f47
mmmim24 <mustaq24@student.sust.edu>
2025-03-24 19:50:10 +0600

    initial files

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <>
+            <div className="flex justify-center items-center p-8 bg-emerald-900">
+                <h1 className="text-3xl text-white font-bold">3 6 9</h1>
+            </div>
+        </>
+    )
+}
+
+export default Header
